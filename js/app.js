5acbba6acccfd967e665bf4621a083e940503ac4
smz1680@gmail.com <smz1680@gmail.com>
2020-04-20 14:30:23 -0700

    adding checkWin Function

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 const keyboradSec = document.getElementById("qwerty");
 const phraseDisplay = document.getElementById("phrase");
 const btnReset = document.querySelector(".btn_reset");
+const overlay = document.getElementById("overlay");
+let title = document.querySelector(".title");
 let geussCount = 0;
 let phrases = [
     "taking a walk",
@@ -14,7 +16,6 @@ let phrases = [
 
 //  events handler to hide the star screem overlay;
 btnReset.addEventListener("click",()=>{
-    let overlay = document.getElementById("overlay");
     overlay.style.display = "none";
 })
 
@@ -65,7 +66,9 @@ addPhraseToDisplay(arrayPhrases);
 keyboradSec.addEventListener("click",(e)=>{
     if(e.target.tagName === "BUTTON"){
         e.target.classList.add("chosen");
+        e.target.disabled = true;
         let choseLetter = e.target.textContent;
+        let letters = document.querySelectorAll(".letter");
         // let letterFound;
         // let letter = document.querySelectorAll(".letter");
         // let choseLetter = e.target.textContent;
@@ -78,7 +81,6 @@ keyboradSec.addEventListener("click",(e)=>{
 //function of checkletter to check and add class show to the list
         function checkLetter(arr){
             let letterFound;
-            let letters = document.querySelectorAll(".letter");
             for(let i = 0; i < letters.length; i ++){
                 if(arr === letters[i].textContent){
                     letters[i].classList.add("show");
@@ -87,19 +89,62 @@ keyboradSec.addEventListener("click",(e)=>{
             }
             return letterFound;
         };
-      let count = 0;
-// adding a statement that is going remove the tries class when the guesses is wrong
+/* 
+adding a statement that is going remove the tries class when the guesses is wrong
+and add  a counter by one with the letter is undefined;
+*/
         if(checkLetter(choseLetter) === undefined){
             let ol = document.querySelector("ol"); 
             let li = ol.firstElementChild;
             ol.removeChild(li);
-            count += 1;
+            geussCount += 1;
         }else{
             checkLetter(choseLetter);
         }
-        console.log(count);
+ /* 
+Adding a statement the is going to check the states of the user guess,if the user guess right,or
+the guesser loose all of it's five points.that is checkwin function.
+*/
+    // let classShow = document.querySelectorAll(".show");
+    // let letterLength = letters.length;
+    // let showLenth = classShow.length;
+
+    // if(letterLength === showLenth){
+    //     overlay.classList.add("win");
+    //     overlay.style.display = "";
+    //     title.textContent = "You Win The Guess!! Yeah!!!";
+    //     btnReset.setAttribute("onClick","window.location.reload()")
+    //     btnReset.textContent = "Start Over";
+    // }else if(geussCount >= 5){
+    //     overlay.classList.add("lose");
+    //     overlay.style.display = "";
+    //     title.textContent = "O,Sorry,Try It One More Time..";
+    //     btnReset.setAttribute("onClick","window.location.reload()")
+    //     btnReset.textContent = "Try One More Time";
+    // }
+
+    function checkWin(arr){
+        let classShow = document.querySelectorAll(".show");
+        let letterLength = letters.length;
+        let showLenth = classShow.length;
+        if(letterLength === showLenth){
+            arr.classList.add("win");
+            arr.style.display = "";
+            title.textContent = "You Win The Guess!! Yeah!!!";
+            btnReset.setAttribute("onClick","window.location.reload()");
+            btnReset.textContent = "Start Over";
+        }else if(geussCount >= 5){
+            arr.classList.add("lose");
+            arr.style.display = "";
+            title.textContent = "O,Sorry,Try It One More Time..";
+            btnReset.setAttribute("onClick","window.location.reload()");
+            btnReset.textContent = "Try One More Time";
+        }
+    }
+    checkWin(overlay);
     }
 })
+
 
 
 
